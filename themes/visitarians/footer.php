31e74475b00d796d526d3272252220eae11f9859
banyalsusheel <banyalsusheel@gmail.com>
2018-10-10 02:35:34 +0530

    popper js added

--- themes/visitarians/footer.php
+++ themes/visitarians/footer.php
@@ -195,12 +195,8 @@
 </footer>	
 <!-- End footer Area -->			
 <?php wp_footer(); ?>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-<!-- <script src="<?php //echo get_stylesheet_directory_uri();?>/assets/js/jquery-2.2.4.min.js"></script> -->
-
+<script src="<?php echo get_stylesheet_directory_uri();?>/assets/js/popper.min.js" ></script>
 <script src="<?php echo get_stylesheet_directory_uri();?>/assets/js/bootstrap.min.js"></script>
-
-<!-- <script src="<?php //echo get_stylesheet_directory_uri();?>/assets/js/jquery.magnific-popup.min.js"></script>	 -->
 <script src="<?php echo get_stylesheet_directory_uri();?>/assets/js/owl.carousel.min.js"></script>			
 <script src="<?php echo get_stylesheet_directory_uri();?>/assets/js/jquery.sticky.js"></script>
 
